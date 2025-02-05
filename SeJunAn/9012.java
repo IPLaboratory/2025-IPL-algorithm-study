e5829251c3d1ec86bb874bdf115be79a3dc209b8
SeJunAn <blossom3912@naver.com>
2025-02-05 14:46:54 +0900

    [9012 - solve] 괄호

--- SeJunAn/9012.java
+++ SeJunAn/9012.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Stack;
-
+// 커밋 메시지 수정
 public class test12312 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
